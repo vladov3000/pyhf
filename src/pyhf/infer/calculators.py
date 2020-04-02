f5520c57a50167077752dd2f4b4ecc5afb27ffc1
Matthew Feickert <matthew.feickert@cern.ch>
2020-04-01 23:57:06 -0500

    Add examples for AsymptoticTestStatDistribution

--- src/pyhf/infer/calculators.py
+++ src/pyhf/infer/calculators.py
@@ -76,6 +76,13 @@ class AsymptoticTestStatDistribution(object):
         """
         Compute the :math:`p`-value for a given value of the test statistic.
 
+        Example:
+
+            >>> import pyhf
+            >>> bkg_dist = pyhf.infer.calculators.AsymptoticTestStatDistribution(0.0)
+            >>> bkg_dist.pvalue(0)
+            0.5
+
         Args:
             value (`float`): The test statistic value.
 
@@ -89,6 +96,14 @@ class AsymptoticTestStatDistribution(object):
     def expected_value(self, nsigma):
         """
         Return the expected value of the test statistic.
+
+        Example:
+
+            >>> import pyhf
+            >>> bkg_dist = pyhf.infer.calculators.AsymptoticTestStatDistribution(0.0)
+            >>> n_sigma = pyhf.tensorlib.astensor([-2, -1, 0, 1, 2])
+            >>> bkg_dist.expected_value(n_sigma)
+            array([-2., -1.,  0.,  1.,  2.])
 
         Args:
             nsigma (`int` or `tensor`): The number of standard deviations.
